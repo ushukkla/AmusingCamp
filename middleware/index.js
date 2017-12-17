62ba36c50170598e92004206d5aea3e5edce2954
Utkarsh shukla <utkarshthehellmaster@gmail.com>
2017-12-17 23:54:27 +0530

    Added new functionalities like admin authentication

--- middleware/index.js
+++ middleware/index.js
@@ -1,58 +1,57 @@
-var Campground = require("../models/campground"),
-    Comment = require("../models/comment");
-
-// all the middleware goes here
-var middlewareObj = {};
-
-middlewareObj.checkCampgroundOwnership = function(req, res, next) {
- if(req.isAuthenticated()){
-        Campground.findById(req.params.id, function(err, foundCampground){
-           if(err){
-               req.flash("error", "Campground not found");
-               res.redirect("back");
-           }  else {
-               // does user own the campground?
-            if(foundCampground.author.id.equals(req.user._id)) {
-                next();
-            } else {
-                req.flash("error", "You don't have permission to do that");
-                res.redirect("back");
-            }
-           }
-        });
+var Comment = require('../models/comment'),
+    Campground = require('../models/campground');
+module.exports = {
+  isLoggedIn: function(req, res, next){
+      if(req.isAuthenticated()){
+          return next();
+      }
+      req.flash('error', 'You must be signed in to do that!');
+      res.redirect('/login');
+  },
+  checkUserCampground: function(req, res, next){
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if(err || !foundCampground){
+          console.log(err);
+          req.flash('error', 'Sorry, that campground does not exist!');
+          res.redirect('/campgrounds');
+      } else if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
+          req.campground = foundCampground;
+          next();
+      } else {
+          req.flash('error', 'You don\'t have permission to do that!');
+          res.redirect('/campgrounds/' + req.params.id);
+      }
+    });
+  },
+  checkUserComment: function(req, res, next){
+    Comment.findById(req.params.commentId, function(err, foundComment){
+       if(err || !foundComment){
+           console.log(err);
+           req.flash('error', 'Sorry, that comment does not exist!');
+           res.redirect('/campgrounds');
+       } else if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
+            req.comment = foundComment;
+            next();
+       } else {
+           req.flash('error', 'You don\'t have permission to do that!');
+           res.redirect('/campgrounds/' + req.params.id);
+       }
+    });
+  },
+  isAdmin: function(req, res, next) {
+    if(req.user.isAdmin) {
+      next();
     } else {
-        req.flash("error", "You need to be logged in to do that");
-        res.redirect("back");
+      req.flash('error', 'This site is now read only thanks to spam and trolls.');
+      res.redirect('back');
     }
-}
-
-middlewareObj.checkCommentOwnership = function(req, res, next) {
- if(req.isAuthenticated()){
-        Comment.findById(req.params.comment_id, function(err, foundComment){
-           if(err){
-               res.redirect("back");
-           }  else {
-               //user comments
-            if(foundComment.author.id.equals(req.user._id)) {
-                next();
-            } else {
-                req.flash("error", "You don't have permission to do that");
-                res.redirect("back");
-            }
-           }
-        });
-    } else {
-        req.flash("error", "You need to be logged in to do that");
-        res.redirect("back");
+  },
+  isSafe: function(req, res, next) {
+    if(req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
+      next();
+    }else {
+      req.flash('error', 'Only images from images.unsplash.com allowed.\nSee https://youtu.be/Bn3weNRQRDE for how to copy image urls from unsplash.');
+      res.redirect('back');
     }
+  }
 }
-//middle
-middlewareObj.isLoggedIn = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You need to be logged in to do that");
-    res.redirect("/login");
-}
-
-module.exports = middlewareObj;
